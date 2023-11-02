b0a6acff15b4fd746ae18d126a01c8352e8139b4
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-02 11:08:24 +0200

    *Refactor git ignore file *Add functions for adding, displaying, editing, and completing todos.

--- functions.py
+++ functions.py
@@ -43,3 +43,63 @@ def read_todos(file_path: str = "todos.txt") -> list[str]:
     with open(file_path, "r") as file:
         todos = file.readlines()
     return todos
+
+
+def add_todo(todo):
+    """
+    Add a todo to the list.
+
+    Args:
+        todo (str): The todo to add.
+    """
+    todos = read_todos()
+    todos.append(todo.capitalize() + "\n")
+    write_todos(todos)
+
+
+def display_todos():
+    """
+    Display the list of todos.
+    """
+    todos = read_todos()
+    for index, item in enumerate(todos):
+        print(f"{index + 1}. {item.strip()}")
+
+
+def edit_todo(action):
+    """
+    Edit a todo in the list.
+
+    Args:
+        action (str): The action to perform.
+    """
+    number = get_position(
+        action, "Please, enter a position to edit (must be a number): "
+    )
+    try:
+        todos = read_todos()
+        new_todo = input("Enter a new todo: ")
+        todos[number] = new_todo.capitalize() + "\n"
+        write_todos(todos)
+    except ValueError:
+        print("Please, enter a valid number.")
+
+
+def complete_todo(action):
+    """
+    Complete a todo in the list.
+
+    Args:
+        action (str): The action to perform.
+    """
+    number = get_position(
+        action, "Please, enter a position to complete (must be a number): "
+    )
+    try:
+        todos = read_todos()
+        todo_to_complete = todos.pop(number).strip()
+        message = f"You have completed {todo_to_complete}"
+        print(message)
+        write_todos(todos)
+    except IndexError:
+        print("Please, enter a valid position.")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import get_position, read_todos, write_todos
+from functions import add_todo, complete_todo, display_todos, edit_todo
 
 
 def main():
@@ -9,48 +9,19 @@ def main():
 
         if action.startswith("add"):
             todo = action.replace("add", "").strip()
-
-            todos = read_todos()
-            todos.append(todo.capitalize() + "\n")
-
-            write_todos(todos)
+            add_todo(todo)
 
         elif action.startswith(("show", "display")):
-            todos = read_todos()
-
-            for index, item in enumerate(todos):
-                print(f"{index + 1}. {item.strip()}")
+            display_todos()
 
         elif action.startswith("exit"):
             break
 
         elif action.startswith("edit"):
-            number = get_position(
-                action, "Please, enter a position to edit (must be a number): "
-            )
-            try:
-                todos = read_todos()
-                todo = todos[number].strip()
-                new_todo = input("Enter a new todo: ")
-                todos[number] = new_todo.capitalize() + "\n"
-                write_todos(todos)
-            except ValueError:
-                print("Please, enter a valid number.")
-                continue
+            edit_todo(action)
 
         elif action.startswith("complete"):
-            number = get_position(
-                action, "Please, enter a position to complete (must be a number): "
-            )
-            try:
-                todos = read_todos()
-                todo_to_complete = todos.pop(number).strip()
-                message = f"You have completed {todo_to_complete}"
-                print(message)
-                write_todos(todos)
-            except IndexError:
-                print("Please, enter a valid position.")
-                continue
+            complete_todo(action)
 
         else:
             print("Hey, you entered an unknown command.")
