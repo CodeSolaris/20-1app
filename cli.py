04f3f763b78aabb547ec1f6c1e8646e0a409715a
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-05 13:07:39 +0200

    - Refactors todo list display functionality by importing required functions and updating function calls - Refactors todo_list functions by removing unnecessary code, improving code structure, and optimizing file operations - Introduces new import statements and a todo_list variable for retrieving a list of todos - Replaces existing list_box and todos variables with new ones - Introduces an exit_button and modifies logic for adding, editing, and completing todos - Updates the window accordingly

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
-from functions import add_todo, complete_todo, display_todos, edit_todo
 from time import strftime
+
+from functions import add_todo, complete_todo, display_todo_list, edit_todo
 
 now = strftime("%B %Y %H:%M:%S")
 print("It's", now)
@@ -16,7 +17,7 @@ def main():
             add_todo(todo)
 
         elif action.startswith(("show", "display")):
-            display_todos()
+            display_todo_list()
 
         elif action.startswith("exit"):
             break
--- gui.py
+++ gui.py
@@ -1,49 +1,59 @@
-import functions
 import PySimpleGUI as sg
 
+import functions
 
-todos = functions.read_todos()
+todo_list = functions.get_todo_list()
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=todos, key="todos", enable_events=True, size=[45, 10])
+list_box = sg.Listbox(values=todo_list, key="todos", enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 window = sg.Window(
     "My To-Do App",
     layout=[
         [label],
         [input_box, add_button],
-        [list_box],
-        [edit_button, complete_button],
+        [list_box, edit_button],
+        [complete_button, exit_button],
     ],
     font=("Helvetica", 18),
 )
+
 
 while True:
     event, values = window.read()
 
     match event:
         case "Add":
-            functions.add_todo(values["todo"])
-            window["todos"].update(values=todos)
+            todo = values["todo"]
+            functions.add_todo(todo)
+            todo_list = functions.get_todo_list()
+            functions.window_update(todo_list, window)
+
+        case "Exit":
+            break
+
         case "Edit":
             todo_to_edit = values["todos"][0]
             new_todo = values["todo"]
 
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
+            index = todo_list.index(todo_to_edit)
+            todo_list[index] = new_todo
+            functions.write_todo_list(todo_list)
+            functions.window_update(todo_list, window)
+
         case "todos":
             window["todo"].update(value=values["todos"][0])
+
         case "Complete":
             todo_to_complete = values["todos"][0]
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            todo_list.remove(todo_to_complete)
+            functions.write_todo_list(todo_list)
+            functions.window_update(todo_list, window)
 
         case sg.WIN_CLOSED:
             break
