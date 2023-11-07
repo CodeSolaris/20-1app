41d18f035500abd80971dda2e28ebcf3246166d4
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-07 23:05:39 +0200

    * feat(web.py): add functionality to add new todo to web app

--- web.py
+++ web.py
@@ -1,25 +1,31 @@
 import streamlit as st
+
 import functions
 
 todo_list = functions.get_todo_list()
 
+
+def add_to_webapp_todolist():
+    todo = st.session_state["new_todo"]
+    functions.add_todo(todo)
+
+
 st.title("My To-Do App")
-st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
 for index, todo in enumerate(todo_list):
     checkbox = st.checkbox(todo, key=todo)
-    # if checkbox:
-    #     todo_list.pop(index)
-    #     functions.write_todo_list(todo_list)
-    #     del st.session_state[todo]
-    #     st.experimental_rerun()
+    if checkbox:
+        confirm = st.button(
+            "Confirm to complete", help="Press confirm to complete this todo"
+        )
+        if confirm:
+            todo_list.pop(index)
+            functions.write_todo_list(todo_list)
+            del st.session_state[todo]
+            st.rerun()
 
-st.button("Add To-Do")
 
-st.text_input(
-    "Add a new todo",
-    placeholder="Add a new todo",
-    key="new_todo",
-    on_change=functions.add_todo,
+new_todo_input = st.text_input(
+    "Add a new todo", key="new_todo", help="Add a new todo to your list"
 )
